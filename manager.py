c890d995c8816bf4bdaa79799726e5b1da2fa38b
swaggyliu <liudiyi951224@163.com>
2021-12-12 23:10:04 +0800

    添加静态文件到info下

--- manager.py
+++ manager.py
@@ -7,7 +7,7 @@ pymysql.install_as_MySQLdb()
 # 通过指定的配置名字创建对应的app
 app = create_app('development')
 manager = Manager(app)
-# 将app与db关联
+# 将app与db关联static
 Migrate(app, db)
 # 将数据库迁移命令添加到manager中
 manager.add_command('db', MigrateCommand)
